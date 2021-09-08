9028eaa57efd594cff38297c7a12f80686bef1c7
ayman <aymanjaber2012@gmail.com>
2021-09-08 12:29:57 +0300

    Add Helper methods for saving token in local storage

---
+++ src/Utils/helpers.js
@@ -0,0 +1,12 @@
+export const login = (token) => {
+    window.localStorage.setItem('token', token);
+};
+
+export const logout = () => {
+    window.localStorage.removeItem('token');
+};
+
+export const getAuthorizeToken = () => {
+    const token = window.localStorage.getItem('token');
+    return token || undefined;
+};
