3937e793bef24486004ba1854ecefffde2e5ef88
ayman <aymanjaber2012@gmail.com>
2021-09-10 14:01:21 +0300

    Finish Initial API Util

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -24,3 +24,50 @@ authorizeApi.interceptors.request.use(
 );
 
 const getData = (res) => res.data;
+
+const requests = {
+    delete: (url) => api.delete(url).then(getData),
+    get: (url) => api.get(url).then(getData),
+    put: (url, body) => api.put(url, body).then(getData),
+    post: (url, body) => api.post(url, body).then(getData),
+};
+
+const authorizeRequests = {
+    delete: (url) => authorizeApi.delete(url).then(getData),
+    get: (url) => authorizeApi.get(url).then(getData),
+    put: (url, body) => authorizeApi.put(url, body).then(getData),
+    post: (url, body) => authorizeApi.post(url, body).then(getData),
+};
+
+
+
+
+
+
+
+
+const auth = {
+    login: (body) => requests.post('/users/login', body).then((data) => {
+        login(data.user.token);
+        return Promise.resolve(data.user);
+    }),
+
+    logout: () => {
+        logout();
+        return Promise.resolve({ user: null });
+    },
+
+    register: (body) => requests.post('/users', body).then((data) => {
+        login(data.user.token);
+        return Promise.resolve(data.user);
+    }),
+
+    user: () => authorizeRequests.get('/user'),
+};
+
+const appointment = {
+    post: (body) => authorizeRequests.post('/appointments', body),
+    get: (id) => authorizeRequests.get(id ? `/appointments/${id}` : '/appointments'),
+};
+
+export { auth, appointment };
